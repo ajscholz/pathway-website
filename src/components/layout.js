ce629858cb14210bf69c9a96a222e11464f719c6
Andrew Scholz <yoitsscholzy@gmail.com>
2019-12-13 08:07:30 -0500

    add paper kit navbar, header, and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+import Navigation from "./navbar"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,33 +26,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div>
-        <main
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </main>
-      </div>
+      <Navigation />
+      <main>{children}</main>
       <Footer />
-      {/* <footer>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </div>
-      </footer> */}
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Image from "components/image"
+import SEO from "components/seo"
+import Header from "components/header"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
+    <Header />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
