737021ed149ba5dbc0bbbb37e27718ff7d5796fa
Andrew Scholz <yoitsscholzy@gmail.com>
2019-12-16 11:25:17 -0500

    update footer styles and link info to contentful

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,59 @@
 /*eslint-disable*/
 import React from "react"
 
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 // reactstrap components
-import { Container, Row } from "reactstrap"
+import { Container, Row, Col, Button } from "reactstrap"
 import SocialButtons from "src/components/buttons/social"
 import ContactButton from "./buttons/contact"
 
 // core components
 
 const Footer = () => {
+  const { info } = useStaticQuery(graphql`
+    {
+      info: contentfulChurchInformation {
+        name
+        street: streetAddress
+        city
+        state
+        zip
+      }
+    }
+  `)
+
   return (
     <>
       <footer className="footer footer-black footer-white pt-4">
         <Container>
-          <Row className="flex-column flex-md-row align-items-center justify-content-md-between">
-            <nav className="footer-nav">
-              <ul>
-                <li className="mb-0">
-                  <Link to="/">Pathway Community Church</Link>
-                </li>
-              </ul>
-            </nav>
-            <ContactButton color="primary" className="m-3" />
-            <SocialButtons className="m-3 align-self-center " />
+          <Row className="align-items-start">
+            <Col md="4" className="text-center text-md-left">
+              <Button
+                tag={Link}
+                size="lg"
+                to="/"
+                className="btn-link text-white px-0 pt-0 mt-3 border-0 text-capitalize font-weight-light"
+              >
+                <strong>{info.name}</strong>
+              </Button>
+              <p>
+                <small>
+                  {info.street}
+                  <br />
+                  {info.city} {info.state} {info.zip}
+                </small>
+              </p>
+            </Col>
+            <Col md="4" className="d-flex justify-content-center my-4 my-md-0">
+              <ContactButton color="primary" className="m-3" />
+            </Col>
+            <Col
+              md="4"
+              className="d-flex justify-content-center justify-content-md-end"
+            >
+              <SocialButtons className="m-3 align-self-center " />
+            </Col>
           </Row>
           <Row>
             <div className="credits m-auto">
