438c2cb23edfaf802ee72e22e84e4c2d7e9986fe
Andrew Scholz <yoitsscholzy@gmail.com>
2019-12-17 23:55:17 -0500

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pathway Community Church`,
+    description: `A church for you in Marietta, Ohio.`,
+    author: `@pathwaychurch`,
+    siteUrl: `https://www.pathwaymarietta.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
