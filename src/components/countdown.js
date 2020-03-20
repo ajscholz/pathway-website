a5d6638f608153e758bcd4f8b8187f3ca2b43875
Andrew Scholz <yoitsscholzy@gmail.com>
2020-03-20 13:56:31 -0400

    improve countdown timer performance and accuracy

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -5,35 +5,35 @@ import { graphql, useStaticQuery } from "gatsby"
 import "./countdown.css"
 
 const Countdown = () => {
-  const setNextSunday = d => {
-    d.setDate(d.getDate() + ((5 + 7 - d.getDay()) % 7))
-    return d
-  }
-  const setNextSundayFromSunday = d => {
-    const days = 7 - d.getDay() + 5
-    const nextSunday = new Date(d.setDate(d.getDate() + days))
-    return nextSunday
-  }
+  // const setNextSunday = d => {
+  //   d.setDate(d.getDate() + ((5 + 7 - d.getDay()) % 7))
+  //   return d
+  // }
+  // const setNextSundayFromSunday = d => {
+  //   const days = 7 - d.getDay() + 5
+  //   const nextSunday = new Date(d.setDate(d.getDate() + days))
+  //   return nextSunday
+  // }
 
-  const setTime = d => {
-    d.setHours(10)
-    d.setMinutes(30)
-    d.setSeconds(0)
-    return d
-  }
+  // const setTime = d => {
+  //   d.setHours(10)
+  //   d.setMinutes(30)
+  //   d.setSeconds(0)
+  //   return d
+  // }
 
-  // if it's after Sunday at 11:30a set to next Sunday
-  const findNextSunday = () => {
-    if (nextStream.getDay() !== 5) {
-      nextStream = setNextSunday(nextStream)
-    } else if (nextStream.getHours() === 11 && nextStream.getMinutes() >= 30) {
-      nextStream = setNextSundayFromSunday(nextStream)
-    } else if (nextStream.getHours() > 11) {
-      nextStream = setNextSundayFromSunday(nextStream)
-    }
-    // now that the date is right, set the time to 10:30am
-    nextStream = setTime(nextStream)
-  }
+  // // if it's after Sunday at 11:30a set to next Sunday
+  // const findNextSunday = () => {
+  //   if (nextStream.getDay() !== 5) {
+  //     nextStream = setNextSunday(nextStream)
+  //   } else if (nextStream.getHours() === 11 && nextStream.getMinutes() >= 30) {
+  //     nextStream = setNextSundayFromSunday(nextStream)
+  //   } else if (nextStream.getHours() > 11) {
+  //     nextStream = setNextSundayFromSunday(nextStream)
+  //   }
+  //   // now that the date is right, set the time to 10:30am
+  //   nextStream = setTime(nextStream)
+  // }
 
   const data = useStaticQuery(graphql`
     {
@@ -58,73 +58,120 @@ const Countdown = () => {
     }
   `)
 
-  // const now = new Date()
-  // let streamData = data.streams.all.find(stream => {
-  //   const d = new Date(stream.dateTime)
-  //   return d > now
-  // })
+  const now = new Date()
+  let streamData = data.streams.all.find(stream => {
+    const d = new Date(stream.dateTime)
+    return d > now
+  })
 
-  // let nextStream = new Date(streamData.dateTime)
+  let next = new Date(streamData.dateTime).getTime()
+  next = new Date(2020, 2, 20, 14).getTime()
 
   // if (typeof streamData === "undefined") {
   //   findNextSunday()
   // }
 
-  let nextStream = new Date(2020, 2, 22, 10, 30)
+  // const next = next2.getTime()
 
-  // initialize state to number of seconds left before next Sunday at 10:30am
-  const [timeLeft, setTimeLeft] = useState(
-    Math.floor(nextStream.getTime() - Date.now())
-  )
+  const [time, setTime] = useState(next - Date.now())
 
   useEffect(() => {
-    if (timeLeft > 0) {
-      setTimeout(() => {
-        setTimeLeft(timeLeft - 1000)
-      }, 1000)
-    }
-  }, [timeLeft])
+    setTimeout(() => {
+      setTime(next - Date.now())
+    }, 1000)
+  }, [time])
+
+  // let nextStream = next
+  const timeLeft = next - Date.now()
+
+  // initialize state to number of seconds left before next Sunday at 10:30am
+  // const [timeLeft, setTimeLeft] = useState(
+  //   Math.floor(nextStream.getTime() - Date.now())
+  // )
+
+  // useEffect(() => {
+  //   if (timeLeft > 0) {
+  //     setTimeout(() => {
+  //       setTimeLeft(timeLeft - 1000)
+  //     }, 1000)
+  //   }
+  // }, [timeLeft])
 
   return (
     <>
+      {/* <div>
+        <h2>{time}</h2>
+        {time > 0 && (
+          <h2>
+            {Math.floor(time / 86400000)
+              .toString()
+              .padStart(2, "0")}
+            <span className="colon" />
+            {Math.floor((time / 3600000) % 24)
+              .toString()
+              .padStart(2, "0")}
+            <span className="colon" />
+            {Math.floor((time / 60000) % 60)
+              .toString()
+              .padStart(2, "0")}
+            <span className="colon" />
+            {Math.floor((time / 1000) % 60)
+              .toString()
+              .padStart(2, "0")}
+          </h2>
+        )}
+        <h2>{next - Date.now()}</h2>
+      </div> */}
+
       <div className="countdownContainer mt-4 d-flex justify-content-center text-light">
         {timeLeft > 0 ? (
           <div className="countdownGrid">
-            <div className="h4 title">{`streaming in`}</div>
+            <div className="h4 streamingTitle">{`streaming in`}</div>
             <div className="timer days">
-              {Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+              {Math.floor(time / 86400000)
                 .toString()
                 .padStart(2, "0")}
               <div className="label" aria-label="days">
                 dys
               </div>
             </div>
-            <div className="timer colon">:</div>
+            <span className="timer colon" />
             <div className="timer hours">
-              {Math.floor((timeLeft / (1000 * 60 * 60)) % 24)
+              {Math.floor((time / 3600000) % 24)
                 .toString()
                 .padStart(2, "0")}
               <div className="label" aria-label="hours">
                 hrs
               </div>
             </div>
-            <div className="timer colon">:</div>
+            <span className="timer colon" />
             <div className="timer minutes">
-              {Math.floor((timeLeft / 1000 / 60) % 60)
+              {Math.floor((time / 60000) % 60)
                 .toString()
                 .padStart(2, "0")}
               <div className="label" aria-label="minutes">
                 mns
               </div>
             </div>
-            <div className="timer colon">:</div>
+            <span className="timer colon" />
             <div className="timer seconds">
-              {Math.floor((timeLeft / 1000) % 60)
+              {Math.floor((time / 1000) % 60)
                 .toString()
                 .padStart(2, "0")}
               <div className="label" aria-label="seconds">
                 scs
               </div>
+            </div>
+            <div className="lastStreamButton">
+              <Button
+                color="primary"
+                size="lg"
+                className="mt-5"
+                href={`/messages/series${data.message.series.fields.slug}${data.message.fields.slug}`}
+              >
+                <i className="nc-icon nc-button-play mr-2" />
+                Watch Last Stream
+              </Button>
             </div>
           </div>
         ) : (
@@ -148,16 +195,8 @@ const Countdown = () => {
           </div>
         )}
       </div>
-      {timeLeft > 0 && (
-        <Button
-          color="primary"
-          size="lg"
-          className="mt-5"
-          href={`/messages/series${data.message.series.fields.slug}${data.message.fields.slug}`}
-        >
-          Watch most recent message
-        </Button>
-      )}
+      <h1>{streamData.videoId}</h1>
+      <h1>{streamData.videoId - 1}</h1>
     </>
   )
 }
