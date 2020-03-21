166ef679543eff50026996d4932d845883e61453
Andrew Scholz <yoitsscholzy@gmail.com>
2020-03-21 15:32:29 -0400

    only show on sundays

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ const IndexPage = props => {
         background={image}
         full={true}
         countdown={true}
-        override={day.getDay() === 6 && index !== -1}
+        override={day.getDay() === 0 && index !== -1}
       >
         <div
           className="position-absolute h-100 w-100 d-flex justify-content-center align-items-center"
