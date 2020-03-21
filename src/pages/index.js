c3d248cdbf87b0e55a79ce562412a9e636f0c8ab
Andrew Scholz <yoitsscholzy@gmail.com>
2020-03-21 15:46:08 -0400

    fix responsive scaling on vimeo videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,9 +72,9 @@ const IndexPage = props => {
                       // border: "6px solid rgba(256,256,256,.5",
                       top: 0,
                       left: 0,
-                      right: 0,
-                      bottom: 0,
                     }}
+                    width="100%"
+                    height="100%"
                     controls={true}
                     // light={true}
                   />
