46dc1d807a716320984c865b88552abe37ab1a4d
Andrew Scholz <yoitsscholzy@gmail.com>
2020-03-28 19:08:37 -0400

    fix date to ready for sunday

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = props => {
   // sets up an interval to minimize re-rendering
   useEffect(() => {
     // set start time to 10:30am in minutes
-    const minutesStart = 19 * 60 + 7
+    const minutesStart = 10 * 60 + 30
     const interval = setInterval(() => {
       let d = new Date()
       const minutesNow = d.getHours() * 60 + d.getMinutes()
