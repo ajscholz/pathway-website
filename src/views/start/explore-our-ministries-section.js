2325fab3382a7c4b9e85d1367fa82e4c7892346d
ajscholz <yoitsscholzy@gmail.com>
2020-07-22 14:37:09 -0400

    fix style issue on start page

--- src/views/start/explore-our-ministries-section.js
+++ src/views/start/explore-our-ministries-section.js
@@ -56,138 +56,136 @@ const ExploreOurMinistriesSection = () => {
   const kids = section.linkedContent[1]
   const groups = section.linkedContent[2]
   return (
-    <section className="section section-project cd-section">
-      <div className="projects-2 section section-dark">
-        <Container>
-          <Row>
-            <Col className="ml-auto mr-auto text-center" md="8">
-              <h2 className="title">{section.title}</h2>
-              {/* <h5 className="description">
+    <section className="section section-project cd-section section section-dark">
+      {/* <div className="projects-2 "> */}
+      <Container>
+        <Row>
+          <Col className="ml-auto mr-auto text-center" md="8">
+            <h2 className="title">{section.title}</h2>
+            {/* <h5 className="description">
                 This is the paragraph where you can write more details about
                 your projects. Keep you user engaged by providing meaningful
                 information.
               </h5> */}
-            </Col>
-          </Row>
-          <div className="space-top" />
-          <Row>
-            <Col md="4">
-              <Card className="card-plain">
-                <CardImg
-                  top
-                  tag="div"
-                  className="bg-white img-rounded"
-                  style={{ maxHeight: "210px" }}
-                >
-                  <Image
-                    style={{
-                      maxHeight: "100%",
-                      maxWidth: "210px",
-                      margin: "0 auto",
-                    }}
-                    imgStyle={{ maxHeight: "100%" }}
-                    alt={students.references[0].logo.title}
-                    className="img"
-                    fluid={students.references[0].logo.fluid}
-                  />
-                </CardImg>
-                <CardBody>
-                  <CardTitle tag="h4">{students.title}</CardTitle>
-                  <br />
-                  <h6 className="card-category text-muted">
-                    {students.references[0].ageRange}
-                  </h6>
-                  <div className="card-description">
-                    <MDXRenderer>
-                      {students.description.childMdx.body}
-                    </MDXRenderer>
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="4">
-              <Card className="card-plain">
-                <CardImg
-                  top
-                  tag="div"
-                  className="bg-white img-rounded"
-                  style={{ maxHeight: "210px" }}
-                >
-                  <Image
-                    style={{
-                      maxHeight: "100%",
-                      maxWidth: "210px",
-                      margin: "0 auto",
-                    }}
-                    imgStyle={{ maxHeight: "100%" }}
-                    alt={kids.references[0].logo.title}
-                    className="img"
-                    fluid={kids.references[0].logo.fluid}
-                  />
-                </CardImg>
-                <CardBody>
-                  <CardTitle tag="h4">{kids.title}</CardTitle>
-                  <br />
-                  <h6 className="card-category text-muted">
-                    {kids.references[0].ageRange}
-                  </h6>
-                  <div className="card-description">
-                    <MDXRenderer>{kids.description.childMdx.body}</MDXRenderer>
-                  </div>
-                  <LinkButton
-                    button={kids.callToAction}
-                    color="primary"
-                    className="mt-3"
-                    outline
-                  />
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="4">
-              <Card className="card-plain">
-                <CardImg
-                  top
-                  tag="div"
-                  className="bg-white img-rounded"
+          </Col>
+        </Row>
+        <div className="space-top" />
+        <Row>
+          <Col md="4">
+            <Card className="card-plain">
+              <CardImg
+                top
+                tag="div"
+                className="bg-white img-rounded"
+                style={{ maxHeight: "210px" }}
+              >
+                <Image
                   style={{
-                    maxHeight: "210px",
+                    maxHeight: "100%",
+                    maxWidth: "210px",
+                    margin: "0 auto",
                   }}
-                >
-                  <Image
-                    style={{
-                      maxHeight: "100%",
-                      maxWidth: "210px",
-                      margin: "0 auto",
-                    }}
-                    imgStyle={{ maxHeight: "100%" }}
-                    alt={groups.references[0].logo.title}
-                    className="img"
-                    fluid={groups.references[0].logo.fluid}
-                  />
-                </CardImg>
-                <CardBody>
-                  <CardTitle tag="h4">{groups.title}</CardTitle>
-                  <br />
-                  <h6 className="card-category text-muted">
-                    {groups.references[0].ageRange}
-                  </h6>
-                  <div className="card-description">
-                    <MDXRenderer>
-                      {groups.description.childMdx.body}
-                    </MDXRenderer>
-                  </div>
-                  <LinkButton
-                    button={groups.callToAction}
-                    color="primary"
-                    className="mt-3"
-                    outline
-                  />
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+                  imgStyle={{ maxHeight: "100%" }}
+                  alt={students.references[0].logo.title}
+                  className="img"
+                  fluid={students.references[0].logo.fluid}
+                />
+              </CardImg>
+              <CardBody>
+                <CardTitle tag="h4">{students.title}</CardTitle>
+                <br />
+                <h6 className="card-category text-muted">
+                  {students.references[0].ageRange}
+                </h6>
+                <div className="card-description">
+                  <MDXRenderer>
+                    {students.description.childMdx.body}
+                  </MDXRenderer>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="4">
+            <Card className="card-plain">
+              <CardImg
+                top
+                tag="div"
+                className="bg-white img-rounded"
+                style={{ maxHeight: "210px" }}
+              >
+                <Image
+                  style={{
+                    maxHeight: "100%",
+                    maxWidth: "210px",
+                    margin: "0 auto",
+                  }}
+                  imgStyle={{ maxHeight: "100%" }}
+                  alt={kids.references[0].logo.title}
+                  className="img"
+                  fluid={kids.references[0].logo.fluid}
+                />
+              </CardImg>
+              <CardBody>
+                <CardTitle tag="h4">{kids.title}</CardTitle>
+                <br />
+                <h6 className="card-category text-muted">
+                  {kids.references[0].ageRange}
+                </h6>
+                <div className="card-description">
+                  <MDXRenderer>{kids.description.childMdx.body}</MDXRenderer>
+                </div>
+                <LinkButton
+                  button={kids.callToAction}
+                  color="primary"
+                  className="mt-3"
+                  outline
+                />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="4">
+            <Card className="card-plain">
+              <CardImg
+                top
+                tag="div"
+                className="bg-white img-rounded"
+                style={{
+                  maxHeight: "210px",
+                }}
+              >
+                <Image
+                  style={{
+                    maxHeight: "100%",
+                    maxWidth: "210px",
+                    margin: "0 auto",
+                  }}
+                  imgStyle={{ maxHeight: "100%" }}
+                  alt={groups.references[0].logo.title}
+                  className="img"
+                  fluid={groups.references[0].logo.fluid}
+                />
+              </CardImg>
+              <CardBody>
+                <CardTitle tag="h4">{groups.title}</CardTitle>
+                <br />
+                <h6 className="card-category text-muted">
+                  {groups.references[0].ageRange}
+                </h6>
+                <div className="card-description">
+                  <MDXRenderer>{groups.description.childMdx.body}</MDXRenderer>
+                </div>
+                <LinkButton
+                  button={groups.callToAction}
+                  color="primary"
+                  className="mt-3"
+                  outline
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      {/* </div> */}
     </section>
   )
 }
