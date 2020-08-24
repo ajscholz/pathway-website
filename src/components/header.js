79d62796ea72cc040a40e156ba79b96ab6d69436
ajscholz <yoitsscholzy@gmail.com>
2020-08-24 11:45:09 -0400

    update sunday stream to 6a

--- src/components/header.js
+++ src/components/header.js
@@ -23,14 +23,13 @@ const Header = ({ title, subtitle, background, full, xs, xxs }) => {
 
   // sets up an interval to minimize re-rendering
   useEffect(() => {
-    // set start time to 10:25am in minutes
-    const minutesStart = 10 * 60 + 25
-    // const minutesStart = 7 * 60  // for testing
+    // set start time to 6am in minutes
+    const minutesStart = 6 * 60
     const interval = setInterval(() => {
       let d = new Date()
       const minutesNow = d.getHours() * 60 + d.getMinutes()
 
-      // check if it's Sunday and after 10:25a
+      // check if it's Sunday and after 6a
       if (d.getDay() === 0 && minutesNow >= minutesStart) {
         // if yes, update state and clear the interval
         setShowVideo(true)
@@ -46,10 +45,8 @@ const Header = ({ title, subtitle, background, full, xs, xxs }) => {
   let index = -1
   if (showVideo === true) {
     const today = new Date().toDateString()
-    index = streams.all.findIndex(
-      stream =>
-        new Date(stream.dateTime).toDateString() === today ? true : false
-      // new Date(stream.dateTime).toDateString() >= today ? true : false    // for testing
+    index = streams.all.findIndex(stream =>
+      new Date(stream.dateTime).toDateString() === today ? true : false
     )
   }
   const stream = index === -1 ? {} : streams.all[index]
