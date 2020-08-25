6437eb07ebf3a429431419110b03385d83fc65a1
ajscholz <yoitsscholzy@gmail.com>
2020-08-25 10:34:51 -0400

    complete basic sg & mbti pages

--- src/pages/resources/mbti.js
+++ src/pages/resources/mbti.js
@@ -2,9 +2,12 @@ import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../../components/seo"
 import Header from "../../components/header"
+import { Container, Row, Col, Button } from "reactstrap"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import BreadcrumbSection from "../../components/BreadcrumbSection"
 
 const MyersBriggsPage = ({ data }) => {
-  const { banner } = data.page
+  const { banner, sections } = data.page
   const { image } = banner
 
   return (
@@ -15,37 +18,51 @@ const MyersBriggsPage = ({ data }) => {
         url="https://pathwaymarietta.com/resources/mbti"
       />
       <Header background={image} xxs={true} />
-      <section>hello from mbti page</section>
-      {/* <section>
-      <Container fluid style={ { padding: "0", margin: "0" } }>
-        <div className="row no-gutters">
-          { sections.map((section, i) => {
-            return (
-              <Col
-                key={ section.id }
-                sm={ 12 }
-                md={ 6 }
-                xl={ 4 }
-                className={
-                  i === 2
-                    ? "order-md-3 order-xl-2"
-                    : i === 3
-                      ? "order-md-2 order-xl-3"
-                      : `order-md-${i}`
-                }
+      <div className="section-gray">
+        <BreadcrumbSection
+          crumbs={[
+            { name: "Resources", link: "/resources" },
+            {
+              name: "Myers-Briggs",
+              link: "",
+              active: true,
+            },
+          ]}
+        />
+      </div>
+      <section className="section section-gray">
+        <Container>
+          <Row className="justify-content-center">
+            <Col className="d-flex flex-column">
+              <h1 className="h2 title text-center mt-0">{sections[0].title}</h1>
+              <MDXRenderer>{sections[0].description.childMdx.body}</MDXRenderer>
+              <Button
+                color="primary"
+                className="mt-3 mx-auto"
+                onClick={() => alert("opening assessment")}
               >
-                <ButtonCard
-                  sectionData={ sections[i] }
-                  className="mb-0"
-                  button={ i === 0 && "solid" }
-                  buttonSize={ i === 0 && "lg" }
-                />
+                Take Assessment
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section section-dark">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="h2 title text-center mt-0">{sections[1].title}</h1>
+            </Col>
+            {/* {videos.map(video => (
+              <Col md="6" lg="4" key={video.id} className="mb-4">
+                <Link to={`/resources/help-me-understand${video.fields.slug}`}>
+                  <HelpMeUnderstandVideoCard videoData={video} />
+                </Link>
               </Col>
-            )
-          }) }
-        </div>
-      </Container>
-    </section> */}
+            ))} */}
+          </Row>
+        </Container>
+      </section>
     </>
   )
 }
@@ -56,6 +73,19 @@ export const data = graphql`
   {
     page: contentfulPages(title: { eq: "MBTI" }) {
       ...HeaderFragment
+      sections {
+        ... on ContentfulInformationSection {
+          title
+          description {
+            childMdx {
+              body
+            }
+          }
+        }
+        ... on ContentfulPageSection {
+          title
+        }
+      }
     }
   }
 `
--- src/pages/resources/spiritual-gifts.js
+++ src/pages/resources/spiritual-gifts.js
@@ -2,40 +2,68 @@ import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../../components/seo"
 import Header from "../../components/header"
+import BreadcrumbSection from "../../components/BreadcrumbSection"
+import { Container, Row, Col, Button } from "reactstrap"
+// import HelpMeUnderstandVideoCard from "../../components/cards/HelpMeUnderstandVideoCard"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
 const SpiritualGiftsPage = ({ data }) => {
-  const { banner } = data.page
+  const { banner, sections } = data.page
   const { image } = banner
 
   return (
     <>
       <SEO
-        title="Resources"
+        title="Spiritual Gifts Resources"
         image={image.file.url}
         url="https://pathwaymarietta.com/resources/spiritual-gifts"
       />
       <Header background={image} xxs={true} />
-      <section>hello from spiritual gifts page</section>
-      {/* <section className="blog-2 section section-gray">
+      <div className="section-gray">
         <BreadcrumbSection
           crumbs={[
             { name: "Resources", link: "/resources" },
-            { name: "Help Me Understand Videos", link: "", active: true },
+            {
+              name: "Spiritual Gifts",
+              link: "",
+              active: true,
+            },
           ]}
         />
+      </div>
+      <section className="section section-gray">
         <Container>
-          <h1 className="h2 title text-center">{sections[0].title}</h1>
           <Row className="justify-content-center">
-            {videos.map(video => (
+            <Col className="d-flex flex-column">
+              <h1 className="h2 title text-center mt-0">{sections[0].title}</h1>
+              <MDXRenderer>{sections[0].description.childMdx.body}</MDXRenderer>
+              <Button
+                color="primary"
+                className="mt-3 mx-auto"
+                onClick={() => alert("opening assessment")}
+              >
+                Take Assessment
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section section-dark">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="h2 title text-center mt-0">{sections[1].title}</h1>
+            </Col>
+            {/* {videos.map(video => (
               <Col md="6" lg="4" key={video.id} className="mb-4">
                 <Link to={`/resources/help-me-understand${video.fields.slug}`}>
                   <HelpMeUnderstandVideoCard videoData={video} />
                 </Link>
               </Col>
-            ))}
+            ))} */}
           </Row>
         </Container>
-      </section> */}
+      </section>
     </>
   )
 }
@@ -43,9 +71,23 @@ const SpiritualGiftsPage = ({ data }) => {
 export default SpiritualGiftsPage
 
 export const data = graphql`
-  {
+  query MyQuery {
     page: contentfulPages(title: { eq: "Spiritual Gifts" }) {
+      title
       ...HeaderFragment
+      sections {
+        ... on ContentfulInformationSection {
+          title
+          description {
+            childMdx {
+              body
+            }
+          }
+        }
+        ... on ContentfulPageSection {
+          title
+        }
+      }
     }
   }
 `
