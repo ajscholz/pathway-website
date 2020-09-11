2332d4520d0f341797c1d1d87df9efff25da87ec
ajscholz <yoitsscholzy@gmail.com>
2020-09-11 11:33:47 -0400

    add delays to animated mbti results

--- src/components/assessments/MBTIResults.js
+++ src/components/assessments/MBTIResults.js
@@ -1,107 +1,63 @@
 import React from "react"
-import { useSprings, animated, config } from "react-spring"
+import { useSprings, useSpring, animated, config } from "react-spring"
 
-const MBTIResults = ({ results }) => {
-  console.log(results)
-
+const MBTIResults = ({}) => {
+  const results = [
+    { type: "E/I", scores: [48, 52], types: ["E", "I"], win: "I", winIndex: 1 },
+    { type: "S/N", scores: [27, 73], types: ["S", "N"], win: "N", winIndex: 1 },
+    { type: "T/F", scores: [32, 68], types: ["T", "F"], win: "F", winIndex: 1 },
+    { type: "J/P", scores: [8, 92], types: ["J", "P"], win: "P", winIndex: 1 },
+  ]
   const dispResult = results
     .map(result => result.win)
     .toString()
     .replace(/,/g, "")
 
-  const reveal = useSprings(
+  const delay = i => i * 700 + 200
+  const fadeIn = useSprings(
     results.length,
-    results.map(result => ({
-      from: { width: "0%", right: result.winIndex === 0 ? "unset" : 0 },
-      to: { width: `${result.scores[result.winIndex]}%` },
+    results.map((result, i) => ({
+      from: { opacity: 0 },
+      to: { opacity: 1 },
       config: config.molasses,
+      delay: delay(i),
     }))
   )
 
-  const bars = reveal.map(props => <animated.div style={props}></animated.div>)
-
-  console.log(bars)
-
-  // const pairs = [
-  //   ["E", "I"],
-  //   ["S", "N"],
-  //   ["T", "F"],
-  //   ["J", "P"],
-  // ]
-
-  // const values = [
-  //   [data.e, data.i],
-  //   [data.s, data.n],
-  //   [data.t, data.f],
-  //   [data.j, data.p],
-  // ]
-
-  // // helper function to calculate the percentages from the raw data
-  // const calcPerc = ([left, right]) => {
-  //   const total = left + right
-  //   const leftPercent = Math.round((left / total) * 100)
-  //   const rightPercent = Math.round((right / total) * 100)
-  //   return [leftPercent, rightPercent]
-  // }
-
-  // get an array of percentages from the data
-  // const percentages = values.map(value => calcPerc(value))
-  // get an array of which side "won"
-  // const sides = percentages.map(perc => (perc[0] >= perc[1] ? 0 : 1))
-  // get a string of the personality type
-  // const result = sides
-  //   .map((side, i) => pairs[i][side])
-  //   .toString()
-  //   .replace(/,/g, "")
-
-  const Bar = ({ children, winIndex, perc }) => (
-    <div
-      className={`h-100 bg-primary d-flex ${
-        winIndex === 0 ? `justify-content-end` : `justify-content-start`
-      } align-items-center rounded`}
-      style={{ width: `${perc}%` }}
-    >
-      <span className="mx-2">{children}</span>
-    </div>
+  const bars = useSprings(
+    results.length,
+    results.map((result, i) => ({
+      from: {
+        width: "0%",
+        right: result.winIndex === 0 ? "unset" : 0,
+        opacity: 0,
+      },
+      to: { width: `${result.scores[result.winIndex]}%`, opacity: 1 },
+      config: config.molasses,
+      delay: delay(i),
+    }))
   )
-
-  // const LeftBar = ({ children, perc }) => (
-  //   <div className={`${barStyles}`} style={{ width: `${perc}%` }}>
-  //     <span className="mx-2 ml-auto">{children}</span>
-  //   </div>
-  // )
-
-  // const RightBar = ({ children, perc }) => (
-  //   <div className={`${barStyles}`} style={{ width: `${perc}%` }}>
-  //     <span className="mx-2 mr-auto">{children}</span>
-  //   </div>
-  // )
 
   return (
     <>
       <p className="h5 text-muted mb-0 font-bold">You are most likely an</p>
       <p className="h2 mt-2 mb-4">{dispResult}</p>
-      {reveal.map(props => (
-        <animated.div style={props}></animated.div>
-      ))}
       <div className="w-100">
-        {reveal.map((props, index) => {
-          const pair = results[index]
-
+        {results.map((pair, index) => {
           return (
             <div
               key={pair.type}
               className="position-relative d-flex justify-content-between align-items-center mt-3"
             >
               {pair.types.map((letter, i) => (
-                <div
+                <animated.div
                   className={`h5 mb-0 mt-0 ${i === 0 ? "mr-2" : "ml-2"}${
                     pair.winIndex !== i ? " text-very-muted" : ""
                   }`}
-                  style={{ zIndex: 50 }}
+                  style={{ zIndex: 50, ...fadeIn[index] }}
                 >
                   {letter}
-                </div>
+                </animated.div>
               ))}
               <div
                 className="position-absolute h-100"
@@ -116,7 +72,7 @@ const MBTIResults = ({ results }) => {
                       ? `justify-content-end`
                       : `justify-content-start`
                   } align-items-center rounded`}
-                  style={props}
+                  style={bars[index]}
                 >
                   <span className="mx-2">
                     {`${pair.scores[pair.winIndex]}%`}
